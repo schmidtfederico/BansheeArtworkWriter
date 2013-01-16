e3abec584cf9cc14e8f62e63fcd7c97ae864e3de
federico <federico@fede>
2013-01-16 13:12:50 -0300

    Add "bin" folder.

--- src/bansheeartwork/BansheeArtwork.java
+++ src/bansheeartwork/BansheeArtwork.java
@@ -7,18 +7,20 @@ import java.util.ArrayList;
 public class BansheeArtwork {
 
     public static void main(String[] args) {
-        /* If we have 0 arguments, we run the GUI.
-         * Otherwise, we run the command line interface.
-        */
-        if(args.length != 0){
-            
+        if(args.length > 0){
             ArrayList<String> arguments = new ArrayList<>();
             
             for(String arg : args){
                 if(arg.startsWith("-")){
-                    switch(arg.charAt(1)){
-                        case 'o': AppContext.getInstance().setAllFilesInAFolderWithSameArtwork(true); break;
-                        case 's': AppContext.getInstance().setSkipFilesWithArtwork(true); break;
+                    for(int i = 1; i < arg.length(); i++) {
+                        switch (arg.charAt(i)) {
+                            case 'o':
+                                AppContext.getInstance().setAllFilesInAFolderWithSameArtwork(true);
+                                break;
+                            case 's':
+                                AppContext.getInstance().setSkipFilesWithArtwork(true);
+                                break;
+                        }
                     }
                 }else{
                     arguments.add(arg);
@@ -27,9 +29,7 @@ public class BansheeArtwork {
             CommandLineRun exec = new CommandLineRun(arguments);
             exec.run();
         }else{
-            
-            // GUI.
-            
+            System.out.println("Usage: java -jar BansheeArtwork.jar [-options] mediaLibraryPath [bansheeCachePath]");
         }
     }
 }
